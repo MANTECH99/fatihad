dea6e9fca2023300cea70798c441e8a5a3a2fdc6
Abdoul Ahad Gueye <abdoulahadgueye1999@gmail.com>
2026-07-25 22:52:15 +0000

    first commit 1

--- database/migrations/2026_07_11_132209_create_certifications_table.php
+++ database/migrations/2026_07_11_132209_create_certifications_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
 
-        Schema::create('certification', function (Blueprint $table) {
+        Schema::create('certifications', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('plan'); // trusted_seller, entrepreneur, company
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('certification');
+        Schema::dropIfExists('certifications');
     }
 };
