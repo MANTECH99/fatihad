cc9ef8b150a5c541142f0f2d623976348eef3a03
Abdoul Ahad Gueye <abdoulahadgueye1999@gmail.com>
2026-07-25 23:25:00 +0000

    first commit 2

--- resources/views/pages/privacy.blade.php
+++ resources/views/pages/privacy.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Politique de confidentialité - {{ config('app.name') }}</title>
-    @vite(['resources/css/app.css'])
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-50">
 <div class="max-w-3xl mx-auto px-4 py-12">
